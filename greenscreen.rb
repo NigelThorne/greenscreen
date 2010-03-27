12f78e37c2d4ec585c854b9108803c9dbd605786
Nigel Thorne <git@nigelthorne.com>
2010-03-27 16:18:56 +1100

    I've hacked this to work with TeamCity. I've changed the config to take a regex so you can pick multiple projects at once

--- greenscreen.rb
+++ greenscreen.rb
@@ -4,6 +4,7 @@ require 'erb'
 require 'rexml/document'
 require 'hpricot'
 require 'open-uri'
+require 'nokogiri'
 
 get '/' do
   servers = YAML.load_file 'config.yml'
@@ -12,13 +13,13 @@ get '/' do
   @projects = []
 
   servers.each do |server|
-    xml = REXML::Document.new(open(server["url"], :http_basic_authentication=>[server["username"], server["password"]]))
-    projects = xml.elements["//Projects"]
+    xml = Nokogiri::HTML(open(server["url"]))#, :http_basic_authentication=>[server["username"], server["password"]]))
+    projects = xml.xpath("//table[@class='tcTable']//td[@class='buildConfigurationName']/..")
     
     projects.each do |project|
       monitored_project = MonitoredProject.new(project)
       if server["jobs"]
-        if server["jobs"].detect {|job| job == monitored_project.name}
+        if server["jobs"].detect {|job| /#{job}/ =~ monitored_project.id}
           @projects << monitored_project
         end
       else
@@ -33,19 +34,32 @@ get '/' do
   @columns = 4.0 if @projects.size > 21
   
   @rows = (@projects.size / @columns).ceil
-
+  @rows = 1 if(@rows == 0) 
   erb :index
 end
 
 class MonitoredProject
-  attr_reader :name, :last_build_status, :activity, :last_build_time, :web_url, :last_build_label
+  attr_reader :name, :last_build_status, :activity, :last_build_time, :web_url, :last_build_label, :project, :id, :blame
   
   def initialize(project)
-    @activity = project.attributes["activity"]
-    @last_build_time = Time.parse(project.attributes["lastBuildTime"]).localtime
-    @web_url = project.attributes["webUrl"]
-    @last_build_label = project.attributes["lastBuildLabel"]
-    @last_build_status = project.attributes["lastBuildStatus"].downcase
-    @name = project.attributes["name"]
+  	
+  	buildConfigName = project.xpath(".//td[@class='buildConfigurationName']/a")
+  	
+    # @activity = project.xpath(["activity"]
+    @activity = ""
+
+  	@last_build_time = Time.parse(project.xpath(".//div[@class='teamCityDateTime']").inner_text).localtime
+  	@web_url = buildConfigName.attr("href")
+  	@last_build_label = project.xpath(".//div[@class='teamCityBuildNumber']").inner_text
+
+  	@last_build_status = "success" if buildConfigName.xpath("./..//img").attr("title").value =~ /successful/
+  	@last_build_status = "failure" if buildConfigName.xpath("./..//img").attr("title").value =~ /failing/
+  	@last_build_status = "failure" if buildConfigName.xpath("./..//img").attr("title").value =~ /responsible/
+  	@last_build_status ||= "unknown"
+  	@project = project.xpath("..//div[@class='projectName']/a").inner_text
+  	@name = buildConfigName.inner_text
+  	@id = "#{@project}_#{@name}"
+  	@blame = buildConfigName.xpath("./..//img").attr("title").value	
+  	@blame = (/responsible/ =~ @blame) ? @blame.split(" is ")[0] : nil
   end
 end
---
+++ run.bat
@@ -0,0 +1,1 @@
+c:\ruby\bin\ruby greenscreen.rb
